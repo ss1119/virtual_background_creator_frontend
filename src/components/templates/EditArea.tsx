ba4ccb639924a1c62cb08a6ddda80e35d04bce53
ss1119 <y.sasaki111919@gmail.com>
2022-09-12 13:48:53 +0900

    set background

--- src/components/templates/EditArea.tsx
+++ src/components/templates/EditArea.tsx
@@ -1,3 +1,9 @@
 export const EditArea = () => {
-  return <div className="w-2/3 h-screen bg-blue-200"></div>;
+  return (
+    <div className="w-2/3 h-screen bg-blue-200">
+      <div className="w-full h-5/6 mt-32 px-7">
+        <img src="/assets/background.png" />
+      </div>
+    </div>
+  );
 };
