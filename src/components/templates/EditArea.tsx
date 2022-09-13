431139eb8c76f4437ac265bffc4a7f4438d96445
momongya <mo5e2ka8@gmail.com>
2022-09-14 04:21:21 +0900

    [feature] 背景画像の一時的な置き場のUI設置

--- src/components/templates/EditArea.tsx
+++ src/components/templates/EditArea.tsx
@@ -6,19 +6,20 @@ export const EditArea = () => {
   const exportRef: any = useRef();
 
   return (
-    <div className="w-2/3 h-93">
-      <div className="w-full flex justify-end">
+    <div className="w-2/3 h-93 my-5">
+      <div className="w-full flex justify-between pr-10">
+      <img src="/assets/box.png" className="w-1/2 z-0"></img>
         <DownloadButton
           onClick={() => {
             exportAsImage(exportRef.current, "virtual-background");
           }}
         />
       </div>
-      <div className="flex justify-center mt-10">
+      <div className="flex justify-center">
         <div
           ref={exportRef}
           id="target"
-          className="relative w-11/12 border-8 border-yellow-900"
+          className="relative w-3/4 border-8 border-yellow-900"
         >
           <img src="/assets/background.png" className="z-0"></img>
         </div>
