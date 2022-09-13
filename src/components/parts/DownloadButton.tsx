585d1b5e95344a6a4c7bc13a34e4d23abedf9d3e
ss1119 <y.sasaki111919@gmail.com>
2022-09-13 10:56:20 +0900

    fix download button

--- src/components/parts/DownloadButton.tsx
+++ src/components/parts/DownloadButton.tsx
@@ -4,14 +4,8 @@ type Props = {
 
 export const DownloadButton = (props: Props) => {
   return (
-    <div className="flex justify-end items-center">
-      <button
-        className="text-center mt-3 w-64 py-3"
-        onClick={props.onClick}
-      >
-        <img src="./download-icon.png" className="w-1/6 z-0"></img>
-      </button>
-      
-    </div>
+    <button className="pt-10 pr-12" onClick={props.onClick}>
+      <img src="/assets/download-icon.png" className="w-14 z-0"></img>
+    </button>
   );
 };
--- src/components/parts/DownloadIcon.tsx
+++
@@ -1,7 +0,0 @@
-export const DownloadIcon = () => {
-  return (
-    <div className="mt-8 mr-10 w-14">
-      <img src="/assets/download-icon.svg" />
-    </div>
-  );
-};
