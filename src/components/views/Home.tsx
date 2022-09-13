82de791d4607f9bc4e935248aa2b053096078305
ss1119 <y.sasaki111919@gmail.com>
2022-09-13 17:04:41 +0900

    NFT画像を取得

--- src/components/views/Home.tsx
+++ src/components/views/Home.tsx
@@ -4,19 +4,11 @@ import { EditArea } from "../templates/EditArea";
 import { UploadArea } from "../templates/UploadArea";
 
 export const Home = () => {
-  const [images, setImages] = useState<File[]>([]);
-  const [visible, setVisible] = useState<boolean[]>([]);
-
   return (
     <>
       <Header />
       <div className="flex">
-        <UploadArea
-          images={images}
-          visible={visible}
-          setImages={setImages}
-          setVisible={setVisible}
-        />
+        <UploadArea />
         <EditArea />
       </div>
     </>
