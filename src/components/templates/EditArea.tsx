355b6140a4903be7454167b941293e466c1807b2
ss1119 <y.sasaki111919@gmail.com>
2022-09-13 10:56:30 +0900

    fix editarea

--- src/components/templates/EditArea.tsx
+++ src/components/templates/EditArea.tsx
@@ -11,20 +11,19 @@ export const EditArea = (props: Props) => {
 
   return (
     <div className="w-2/3 h-screen bg-blue-200">
-      <div
-        className="w-full flex justify-end"
-        onClick={() => props.handleOnDeleteButton()}
-      >
+      <div className="w-full flex justify-end">
         <DownloadButton
-          onClick={() => exportAsImage(exportRef.current, "virtual-background")}
-        ></DownloadButton>
-        <div ref={exportRef} className="w-5/6 border-8 border-yellow-900">
+          onClick={() => {
+            props.handleOnDeleteButton();
+            exportAsImage(exportRef.current, "virtual-background");
+          }}
+        />
+      </div>
+      <div className="flex justify-center mt-10">
+        <div ref={exportRef} className="w-11/12 border-8 border-yellow-900">
           <img src="/assets/background.png" className="w-max z-0"></img>
         </div>
       </div>
-      {/* <div className="w-full h-5/6 mt-8 px-7">
-        <img src="/assets/background.png" />
-      </div> */}
     </div>
   );
 };
